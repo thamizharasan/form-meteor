94876758715c5ebd0a1a7c9444c7460bc0170cc1
T-Rex <thamizharasan123@gmail.com>
2015-10-01 21:26:11 +0530

    admin-meteor

---
+++ client/views/admin.js
@@ -0,0 +1,19 @@
+Template.viewsAdmin.onRendered(function(){
+	$('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+
+	$('.dropdown-button').dropdown({
+      inDuration: 400,
+      outDuration: 225,
+      constrain_width: true, // Does not change width of dropdown to that of the activator
+      hover: false, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    }
+  );
+});
